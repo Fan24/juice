506449eeba10ab1bd169247f3b1fdaecf3c35d6d
vultr <dreamsfan@gmail.com>
2019-06-08 22:30:56 +0800

    upate jd pay start shell

---
+++ bin/startJDPay.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+baseDir="/home/ap/fan/OnionMall/"
+config_file="config.json"
+
+PON_PATH=
+if [ "$PYTHONPATH" = "" ];then
+	PON_PATH=$baseDir
+else
+	PON_PATH=$PYTHONPATH:$baseDir
+fi
+export PYTHONPATH=$PON_PATH
+export PROJ_PATH=$baseDir
+export VERIFY_CODE=$baseDir"input/vc.dat"
+export CONF_FILE=$config_file
+cd $baseDir
+srcDir="/home/ap/fan/OnionMall/gy/jd/"
+pythonSrc="PayOrder.py"
+dataDir="/home/ap/fan/OnionMall/data/"
+logDir="/home/ap/fan/OnionMall/log/"
+
+userFn="info.json"
+if [ "$1" ];then
+	userFn=$1
+fi
+nohup python -u $srcDir$pythonSrc $userFn> $logDir/JD_Pay$userFn.log 2>&1 &
+#python $srcDir$pythonSrc father.json
