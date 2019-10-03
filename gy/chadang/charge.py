1e38f217b0ed3a6ccbcde80b1570be5c30cfa9ae
Fan24 <dreamsfan@gmail.com>
2019-10-03 23:15:34 +0800

    还是考拉

--- gy/chadang/charge.py
+++ gy/chadang/charge.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from gy import config
+from gy.netease import KaolaUtil
 import time
 import json
 import datetime
@@ -23,32 +27,146 @@ def load_json(driver):
         return {'errorCode' : '404'}
 
 
-def get_real_phone_recharge(driver, faceValue):
+def kaola_offer(driver, url):
+    driver.execute_script('window.open("https://www.baidu.com");')
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get(url)
+    while not driver.current_url.startswith('https://m-goods.kaola.com'):
+        driver.get(url)
+        time.sleep(2)
+
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.CLASS_NAME, 'mrkprice')))
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.CLASS_NAME, 'nameShop')))
+    return {'price': int(driver.find_element_by_class_name('mrkprice').text[1:]),
+            'shop_name': driver.find_element_by_class_name('nameShop').text}
+
+
+def kaola_do_order(driver, phone):
+    driver.find_element_by_class_name('formbox').submit()
+    time.sleep(2)
+    WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.CLASS_NAME, 'v-input-input')))
+    driver.find_element_by_class_name('v-input-input').send_keys(phone)
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.CLASS_NAME, 'v-submitorderbar-button')))
+    driver.find_element_by_class_name('v-submitorderbar-button').click()
+    WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.CLASS_NAME, 'v-dialog-button-primary')))
+    driver.find_element_by_class_name('v-dialog-button-primary').click()
+    while not driver.current_url.startswith('https://tradenj.kaola.com/order/payway.html'):
+        time.sleep(1)
+    driver.find_element_by_xpath('//input[@value="6"]').click()
+    driver.find_element_by_id('payNow').click()
+    while not driver.current_url.startswith('https://mclient.alipay.com'):
+        time.sleep(1)
+    driver.find_element_by_class_name('J-h5pay').click()
+    time.sleep(1)
+    while True:
+        cur_url = driver.current_url
+        if cur_url.startswith('https://mclient.alipay.com/h5Continue.htm'):
+            break
+    title = driver.find_element_by_class_name('title-main').text
+    if title != "付款详情":
+        driver.find_element_by_xpath('//a[@seed="v5_need_phonelogin-login"]').click()
+        time.sleep(1)
+        driver.find_element_by_id('logon_id').send_keys('dreamsfan@gmail.com')
+        driver.find_element_by_id('pwd_unencrypt').send_keys('802070')
+        driver.find_element_by_id('cashier').submit()
+        time.sleep(2)
+    #cmd = input('input y to pay with this order')
+    cmd = "y"
+    if cmd == "y":
+        driver.find_element_by_id('cashierPreConfirm').submit()
+        time.sleep(1)
+        pwd = [8,0,2,0,7,0]
+        for word in pwd:
+            driver.find_element_by_id('spwd_unencrypt').send_keys(word)
+            time.sleep(0.3)
+        time.sleep(2)
+        print('wait to pay success')
+        while not driver.current_url.startswith('https://tradenj.kaola.com/order/pay_success.html'):
+            time.sleep(2)
+        driver.close()
+    return True
+
+
+def get_real_phone_recharge(driver, face_value_list, channel, discount):
     """
     costless order
     :param driver:
     :param faceValue:
+    :param channel: "KAO_LA", "TAO_BAO"
     :return:
     """
+    driver.refresh()
+    print(driver.get_cookies())
     jsession = driver.get_cookie('logged')['value']
-    face_key = 'faceValue%d' % faceValue
     cnt = 0
+    settle_rate_url = 'http://api.chadan.cn/product/queryCostProductPrice?JSESSIONID=%s&channel=%s' % (jsession, channel)
+    driver.get(settle_rate_url)
+    result = load_json(driver)
+    if result.get('errorCode') != 200:
+        print('unable to load discount')
+        return
+    if result.get('data')[0]['discount'] < discount:
+        print('unable to satisify discount', result.get('data')[0]['discount'])
+        return
     while True:
         time.sleep(3)
         cnt += 1
         print('#%d to get real phone recharge' % cnt)
-        driver.get('http://www.chadan.cn/order/costOrderPool?JSESSIONID=%s' % jsession)
+        driver.get('http://api.chadan.cn/order/costOrderPool?JSESSIONID=%s&channel=%s' % (jsession, channel))
         result = load_json(driver)
-        if result.get('errorCode') != 200 or result.get('data').get(face_key) <= 0:
+        if result.get('errorCode') != 200:
             continue
-        driver.get('http://www.chadan.cn/order/getDirectCostOrder?JSESSIONID=%s&faceValue=%d' %(jsession, faceValue))
+        face_key = None
+        faceValue = None
+        for fv in face_value_list:
+            face_key = 'faceValue%d' % fv
+            if result.get('data').get(face_key) <= 0:
+                continue
+            faceValue = fv
+            break
+        if faceValue is None:
+            continue
+        driver.get('http://api.chadan.cn/order/getDirectCostOrder?JSESSIONID=%s&faceValue=%d&channel=%s' %(jsession, faceValue, channel))
         result = load_json(driver)
         print(result)
         if result.get('errorCode') == 200:
+            order_id = result.get('data').get('id')
+            place_order_url = result.get('data').get('naughty')
+            url = 'http://api.chadan.cn/order/costOrderWaitRecharge?JSESSIONID=%s&id=%s&faceValue=%d' % (
+                jsession, order_id, faceValue)
+            driver.get(url)
+            phone_result = load_json(driver)
+            phone = None
+            if phone_result.get('errorCode') == 200:
+               phone = phone_result.get('data').get('rechargeAccount')
+            if phone is None:
+                cmd = input('could not find recharge phone, please handle manually')
+                if cmd != "n":
+                    break
+                continue
+            print(phone_result)
             print('Order found with charge value %d' % faceValue)
-            cmd = input('input n for next, other to quit')
-            if cmd != "n":
-                break
+            offer = kaola_offer(driver, place_order_url)
+            if kaola_do_order(driver, phone):
+                print('charge order succeed to make')
+                report_kaola_order(driver, order_id, jsession, faceValue)
+            else:
+                print('Unable to make order and pay')
+            #cmd = input('input n for next, other to quit')
+            #if cmd != "n":
+            #    break
+
+
+def report_kaola_order(driver, chadan_order_id, jsession, faceValue):
+    driver.switch_to.window(driver.window_handles[0])
+    report_url = 'http://api.chadan.cn/order/reportDirectCostOrder?JSESSIONID=%s&id=%s&faceValue=%d' % (
+        jsession, chadan_order_id, faceValue
+    )
+    driver.get(report_url)
+    result = load_json(driver)
+    if result.get('errorCode') == 200:
+        return True
+    return False
 
 
 def go_dashboard(driver, user_info):
@@ -115,7 +233,12 @@ def login(driver, user_info):
 def confirm_order(driver, charge_phone, jession):
     confirm_url = 'http://chadan.wang/wang/makeMoney'
     driver.get(confirm_url)
-    time.sleep(3)
+    time.sleep(2)
+    try:
+        driver.find_element_by_class_name('kfclose').click()
+    except:
+        print('Not kfclose')
+    time.sleep(1)
     driver.get(driver.find_element_by_class_name('success').get_attribute('href'))
     time.sleep(3)
     driver.find_element_by_id('sureReport').click()
@@ -224,8 +347,8 @@ conf = config.GyConfig()
 if conf.is_headless():
     chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-web-security')
-#chrome_options.add_argument(
-#    'user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"')
+chrome_options.add_argument(
+    'user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"')
 chrome_options.add_argument('--lang=zh-CN.UTF-8')
 chrome_options.add_argument('--user-data-dir=%s' % conf.get_chrome_user_dir_by_key('chrome_user_dir_chadang'))
 chrome_options.add_argument('--disable-gpu')
@@ -241,8 +364,9 @@ else:
 
 driver.set_window_size(640, 700)
 try:
-    go_dashboard(driver, conf.get_user_info())
-    order_type_list = ["QR", "MBL_CHRG", "PHN_RECH"];
+    user_info = conf.get_user_info()
+    go_dashboard(driver, user_info)
+    order_type_list = ["QR", "MBL_CHRG", "PHN_RECH", "KAO_LA"];
     order_type = order_type_list[1]
     if order_type == "QR":
         print('Go to get QR order')
@@ -252,17 +376,25 @@ try:
         operator_type.append(ot_array[2])
         #operator_type.append(ot_array[3]), operator_type.append(ot_array[1])
         get_qr_order(driver, amount, operator_type)
-    elif order_type == "PHN_RECH":
+    elif order_type == "KAO_LA":
         faceValue_Array = [50, 100, 200, 300, 500]
-        faceVaule = faceValue_Array[4]
-        get_real_phone_recharge(driver, faceVaule)
+        faceValue = [faceValue_Array[0], faceValue_Array[1]]
+        driver.execute_script('window.open("https://www.baidu.com");')
+        time.sleep(1)
+        driver.switch_to.window(driver.window_handles[1])
+        KaolaUtil.mobile_login(driver, user_info)
+        time.sleep(1)
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
+        get_real_phone_recharge(driver, faceValue, order_type, 9.95)
     else:
-        charge_money = 100
+        charge_money = 20
         ot_array = [None, "MOBILE", "UNICOM", "TELECOM"]
-        operator_type = ot_array[0]
+        operator_type = ot_array[1]
         get_charge_order(driver, charge_money, operator_type)
 except:
     traceback.print_exc()
 finally:
+    driver.close()
     driver.quit()
     print('END.OF.PROG')
